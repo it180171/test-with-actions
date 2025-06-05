339752b90e3f7812cd654b1247795c5429de41d4
daltenhofer <57950128+it180171@users.noreply.github.com>
2025-06-05 09:14:46 +0200

    sfsdfsaf

--- .github/public/script.js
+++ .github/public/script.js
@@ -1,7 +1,17 @@
 // ./public/script.js
-module.exports = (data) => {
-  const { context } = data;
-  console.log("Hello from external script!");
-  console.log("The event was: " + context.eventName);
-  return `Processed event: ${context.eventName}`;
+module.exports = async (dependencies) => {
+  // Destructure the passed-in objects
+  const { github, context, core } = dependencies;
+
+  console.log("[External Script] Hello from the external script!");
+  console.log(`[External Script] The event was: ${context.eventName}`);
+  console.log(`[External Script] Repo: ${context.repo.owner}/${context.repo.repo}`);
+
+  // You can use 'github' for API calls, 'context' for event data, 'core' for utils
+  // For example, to set an output that can be used by later steps in the job:
+  core.setOutput("processed_event", context.eventName);
+
+  // The return value of this function will be available as an output
+  // of the actions/github-script step if you 'return' it in the inline script.
+  return `[External Script] Successfully processed event: ${context.eventName}`;
 };
