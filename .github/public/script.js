4fa468726017b119c6607a0074e7c854766a7e8a
daltenhofer <57950128+it180171@users.noreply.github.com>
2025-06-05 08:51:02 +0200

    ssfd

--- .github/public/script.js
+++ .github/public/script.js
@@ -1,1 +1,7 @@
-console.log("Hello World!")
+// ./public/script.js
+module.exports = (data) => {
+  const { context } = data;
+  console.log("Hello from external script!");
+  console.log("The event was: " + context.eventName);
+  return `Processed event: ${context.eventName}`;
+};
